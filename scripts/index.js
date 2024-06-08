2365f8004c01025ad2efa1aa2eb3b3ab4a7319cd
Elisean <ivan.ielisieiev.97@mail.ru>
2024-06-08 05:57:44 +0300

    create form

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
    
-      const headerBurger = document.querySelector('.header-bottom__burger');
-      const headerBurgerMenu = document.querySelector('.header-bottom__burger-menu');
-      const buttons = document.querySelectorAll('.main-button');
-      headerBurger.addEventListener('click', () => {
-        headerBurger.classList.toggle('open');
-        headerBurgerMenu.classList.toggle('open')
-      });
-
-      buttons.forEach(button =>{
-        button.addEventListener('click',()=>{
-        
-        })
-      })
+  const headerBurger = document.querySelector('.header-bottom__burger');
+  const headerBurgerMenu = document.querySelector('.header-bottom__burger-menu');
+  headerBurger.addEventListener('click', () => {
+    headerBurger.classList.toggle('open');
+    headerBurgerMenu.classList.toggle('open');
+  });
   
+
+  const buttons = document.querySelectorAll('.main-button');
+  const form = document.querySelector('.section-form');
+  const buttonClosed = document.querySelector('.section-form__closed');
+ 
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      form.classList.add('open');
+    });
+  });
+  
+  buttonClosed.addEventListener('click', () => {
+    form.classList.remove('open');
+  });
+  
+  document.addEventListener('click', (event) => {
+    if (!form.contains(event.target) && event.target !== form && event.target.textContent !== 'Contact sales') {
+      form.classList.remove('open');
+    }
+  });
+
+  const inputName = document.getElementById('userName');
+
+  inputName.addEventListener('input', () =>{
+    const currentValue = inputName.value;
+    if(currentValue === ''){
+      console.log(false)
+    }
+  });
+
+
+
+
 })
 
 
